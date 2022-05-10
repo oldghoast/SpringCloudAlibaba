6289f60ceacd960d6e531ec9df347abf950b1b3b
oldghoast <2281901186@qq.com>
2022-05-10 21:25:30 +0800

    20220510

---
+++ order-openfeign/src/main/java/com/java/order/feign/StockFeignService.java
@@ -0,0 +1,15 @@
+package com.java.order.feign;
+
+import org.springframework.cloud.openfeign.FeignClient;
+
+/**
+ * @ClassName StockFeignService
+ * @Description SpringCloudAlibaba
+ * @Author oldghoast
+ * @Date 2022/4/23 12:19
+ * @Version 1.0
+ */
+@FeignClient
+public interface StockFeignService {
+
+}
